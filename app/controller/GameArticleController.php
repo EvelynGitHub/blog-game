42402e00aa5340bf076c81fa6bbfa7310c5d08f5
Evelyn <evelynbrandao15@gmail.com>
2020-12-18 19:11:43 -0300

    Esqueleto das functions de CameArticleController

--- app/controller/GameArticleController.php
+++ app/controller/GameArticleController.php
@@ -2,10 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\GameArticle;
+
 class GameArticleController
 {
-    public function __construct()
+
+    //POST - cria artigo
+    public function create($data = null)
     {
-        echo "Chamou o controller";
+        return json_encode(["name" => "create"]);
+    }
+
+    //PUT - altera artigo
+    public function update($id = 0, $data = null)
+    {
+        return json_encode(["name" => "update"]);
+    }
+
+    public function delete($id = 0)
+    {
+        return json_encode(["name" => "delete"]);
+    }
+
+    public function readById($id = 0)
+    {
+        return json_encode(["name" => "readById"]);
+    }
+
+
+    public function readAll()
+    {
+        return json_encode(["name" => "readAll"]);
     }
 }
