ac55db924437b107dfb83c192ad76b0767a9e7ef
Evelyn <evelynbrandao15@gmail.com>
2020-12-18 16:46:49 -0300

    Configuração inicial da API

--- public/api/index.php
+++ public/api/index.php
@@ -1,3 +1,20 @@
 <?php
 
+
+//INICIO: HEADER
+header("Access-Control-Allow-Origin: *"); //Qualquer site pode acessar
+header("Content-Type: application/json; charset=UTF-8"); //tipo de retorno
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE"); // tipos de verbos http aceitos
+header("Access-Control-Max-Age: 3600"); // Durabilidade Máxima de 1 hora
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"); // Habilita alguma autorização
+// header('Access-Control-Allow-Headers:  X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding'); // Habilita alguma autorização
+//FIM: HEADER
+
 require_once("../../vendor/autoload.php");
+
+$controller = null; //Controller que será chamado
+$param      = null; // Parametros que podem vir na url (Exemplo: /game/:id)
+$data       = null; // Dado que podem vir da requisição (Exemplo: formulários)
+$method     = $_SERVER["REQUEST_METHOD"]; //GET, POST, PUT, PATCH e DELETE
+
+echo $method;
