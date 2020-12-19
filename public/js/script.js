95a2075c7e584c1184519b3bc110f5efcab23f60
Evelyn <evelynbrandao15@gmail.com>
2020-12-19 17:16:45 -0300

    metodo readAll implementado

---
+++ public/js/script.js
@@ -0,0 +1,78 @@
+
+window.onload = function () {
+    console.clear()
+    readAll()
+}
+
+function create(params) {
+    console.log("Create")
+}
+
+function readAll() {
+    console.log("Ler Todos")
+
+    ajax({
+        method: "GET",
+        url: "api/game/",
+        data: null,
+        before() { },
+        success(e) {
+            if (e.list.length == 0) return;
+
+            posts = document.querySelector(".box-posts")
+            posts.innerHTML = ""
+
+            console.log("Success read:", e.list)
+
+            for (let i = 0; i < e.list.length; i++) {
+                html = `<div class="item-post">
+                            <div class="img-post">
+                                <img src="${(e.list[i].videoId == "" ? "./img/console.png" : e.list[i].videoId)}" alt="Imagem do Poste">
+                            </div>
+                            <h4>${e.list[i].title}</h4>
+                            <div class="info-post">                                                    
+                                <button type="submit" class="btn" onclick="update(${e.list[i].id})">Editar</button>
+                                <button type="submit" class="btn" onclick="readById(${e.list[i].id})">Visualiza</button>
+                                <button type="submit" class="btn" onclick="deleteArticle(${e.list[i].id})">Deletar</button>
+                            </div >
+                        </div > `
+                posts.innerHTML += html;
+
+            }
+
+
+        },
+        error() { },
+        complete() {
+            console.log("Complete read")
+        }
+
+    })
+}
+
+function readById(id) {
+    console.log("Ler por ID: ", id)
+}
+
+function update(params) {
+    console.log("Update")
+}
+function deleteArticle(id) {
+    console.log("delete")
+}
+
+function ajax(params) {
+
+    $.ajax({
+        type: params.method,
+        url: params.url,
+        data: params.data,
+        dataType: "json",
+        beforeSend: params.before,
+        success: params.success,
+        error: params.error,
+        complete: params.complete
+    });
+
+    console.log("Entrou no Ajax")
+}
